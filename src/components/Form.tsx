aa7d415a8831dc64d5bf0024e710eae5cd1bb0bf
hikmanisyariful <56625154+hikmanisyariful@users.noreply.github.com>
2023-08-02 04:00:17 +0700

    Responsive Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -16,45 +16,47 @@ export default function Form() {
 
   return (
     <div className="formSection">
-      <FormProvider {...methods}>
-        <h1>Hello</h1>
-        <p>Enter your email and password to login.</p>
-        <form className="formWrapper">
-          <InputField name="email" label="Email" type="email" />
-          <InputField
-            name="password"
-            label="Password"
-            type={showPassword ? 'text' : 'password'}
-            toggleEye={(e) => {
-              setShowPassword(e);
-            }}
-          />
-          <div className="infoWrapper">
-            <Checkbox label="Remember me" name="remember_me" />
-            <Button href="#">Forgot Password?</Button>
-          </div>
-          <div className="buttonSubmit">
-            <Button type="submit" variant="primary">
-              Login
-            </Button>
-            <Button variant="secondary">Sign Up</Button>
-          </div>
-          <div className="divider">
-            <span>Or, login with</span>
-          </div>
-          <div className="buttonAuth">
-            <Button type="submit" variant="secondary" size="small">
-              Facebook
-            </Button>
-            <Button variant="secondary" size="small">
-              Linked In
-            </Button>
-            <Button variant="secondary" size="small">
-              Google
-            </Button>
-          </div>
-        </form>
-      </FormProvider>
+      <div className="formContainer">
+        <FormProvider {...methods}>
+          <h1>Hello</h1>
+          <p>Enter your email and password to login.</p>
+          <form className="formWrapper">
+            <InputField name="email" label="Email" type="email" />
+            <InputField
+              name="password"
+              label="Password"
+              type={showPassword ? 'text' : 'password'}
+              toggleEye={(e) => {
+                setShowPassword(e);
+              }}
+            />
+            <div className="infoWrapper">
+              <Checkbox label="Remember me" name="remember_me" />
+              <Button href="#">Forgot Password?</Button>
+            </div>
+            <div className="buttonSubmit">
+              <Button type="submit" variant="primary">
+                Login
+              </Button>
+              <Button variant="secondary">Sign Up</Button>
+            </div>
+            <div className="divider">
+              <span>Or, login with</span>
+            </div>
+            <div className="buttonAuth">
+              <Button type="submit" variant="secondary" size="small">
+                Facebook
+              </Button>
+              <Button variant="secondary" size="small">
+                Linked In
+              </Button>
+              <Button variant="secondary" size="small">
+                Google
+              </Button>
+            </div>
+          </form>
+        </FormProvider>
+      </div>
     </div>
   );
 }
