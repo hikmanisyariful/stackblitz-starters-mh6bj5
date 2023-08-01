e4d9a54bf4841b0acab907d6a6803be24900e656
hikmanisyariful <56625154+hikmanisyariful@users.noreply.github.com>
2023-08-02 05:45:00 +0700

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,18 @@ import './style.css';
 
 export default function App() {
   return (
-    <div>
-      {/* <HeroSection /> */}
+    <div className="appContainer">
+      <div className="backgroundCover">
+        <div className="cover">
+          <h1>
+            <span>Lorem ipsum dolor si amet</span> consectetur
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
       <Form />
     </div>
   );
